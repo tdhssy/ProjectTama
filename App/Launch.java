a2bc1f15ef9e8eecdb64ca561c6fd09f0a94e3e7
Antoine Fetiveau <fetiveau.antoine@gmail.com>
2022-11-05 12:03:15 +0100

    Ajout de la classe Test et avancement de la fonction main de Launch.

--- App/Launch.java
+++ App/Launch.java
@@ -1,19 +1,8 @@
-import controller.Controller;
-import controller.Test;
 
 public class Launch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if (args.length == 0)
-		{
-			Controller controller = Controller.getController();
-			//controller.startEvent();
-		}else if (args.length == 1 || args[1].equals("-t"))
-		{
-			Test test = new Test();
-			test.startTest();
-		}
 
 	}
 
