b441d54942f6b9fc351d40867abdcdc92abec7dc
Antoine Fetiveau <fetiveau.antoine@gmail.com>
2022-11-17 19:47:20 +0100

    Ajout de JUnit

---
+++ App/test/TamaTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TamaTest {
+    
+    @Test
+    void test(){
+        assertTrue(true);
+    } 
+}
