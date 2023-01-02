890ee09e4b2670dade6b330149f0b837288212c9
Antoine Fetiveau <fetiveau.antoine@gmail.com>
2023-01-02 21:24:40 +0100

    .jar et .sh pour lancer l'app

---
+++ Tamago/StartApp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java --module-path openjfx-19_linux-x64_bin-sdk/javafx-sdk-19/lib --add-modules javafx.controls,javafx.graphics,javafx.base,javafx.fxml,javafx.media,javafx.swing,javafx.web -jar ProjectTama.jar
